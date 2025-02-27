580df7cc492f31028dd539d28c598b839f29999d
Mehmet Can Vardar <mehmetcan.vardar@pulsefin.tech>
2025-02-27 22:56:01 +0300

    Simplify Vite configuration with static HMR host

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,16 @@
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => {
-    const config = {
-        plugins: [
-            laravel({
-                input: ["resources/css/app.css", "resources/js/app.js"],
-                refresh: true,
-            }),
-        ],
-    };
-
-    if (command === "build") {
-        config.server = {
-            hmr: {
-                host: "barkloungetr.com",
-            },
-        };
-    } else {
-        config.server = {
-            hmr: {
-                host: "192.168.1.105",
-            },
-        };
-    }
-
-    return config;
+export default defineConfig({
+    server: {
+        hmr: {
+            host: "barkloungetr.com",
+        },
+    },
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
 });
