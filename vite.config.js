2670b4b9f58d1d0ae489ef42220d84391976778c
Mehmet Can Vardar <mehmetcan.vardar@pulsefin.tech>
2025-02-27 22:30:55 +0300

    Remove Vercel configuration and update Vite HMR host

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "https://barkloungetr.com",
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
