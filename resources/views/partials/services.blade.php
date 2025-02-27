addd6a9037cafbda98da5a9ae5ac5a75564e1a26
Mehmet Can Vardar <mehmetcan.vardar@pulsefin.tech>
2025-02-27 22:39:46 +0300

    Remove unused service icons and update icon path in services view

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -54,8 +54,7 @@
                 <div
                     class="bg-white p-10 rounded-xl text-center font-serif items-center justify-center hover:shadow-lg hover:scale-105 hover:rotate-2 transition-all duration-300 w-full">
                     <div class="flex flex-col items-center justify-center h-32">
-                        <img src="{{ asset('icons/' . $service['icon']) }}" alt="{{ $service['title'] }}"
-                            class="w-20 h-20">
+                        <img src="{{ asset($service['icon']) }}" alt="{{ $service['title'] }}" class="w-20 h-20">
                         <h3 class="text-pet-700 font-bold text-xl uppercase mt-4 mb-4">
                             {{ $service['title'] }}
                         </h3>
